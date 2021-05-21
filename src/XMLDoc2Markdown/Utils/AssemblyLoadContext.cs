847a6470d4e7de5637599fe3bacb2b0e62e3ea40
Adrien Siffermann <adrien@siffermann.me>
2021-05-21 20:07:37 +0200

    Resolve dependencies with a custom AssemblyLoadContext

---
+++ src/XMLDoc2Markdown/Utils/AssemblyLoadContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace XMLDoc2Markdown.Utils
+{
+    internal class AssemblyLoadContext : System.Runtime.Loader.AssemblyLoadContext
+    {
+        private readonly AssemblyDependencyResolver resolver;
+
+        public AssemblyLoadContext(string pluginPath)
+        {
+            this.resolver = new AssemblyDependencyResolver(pluginPath);
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            string assemblyPath = this.resolver.ResolveAssemblyToPath(assemblyName);
+            if (assemblyPath != null)
+            {
+                return this.LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            string libraryPath = this.resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+            if (libraryPath != null)
+            {
+                return this.LoadUnmanagedDllFromPath(libraryPath);
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
